8fd1991223ee2fab98524953e9a1b8abbae24243
AnandSVaidya <vaidya.anands@gmail.com>
2019-04-14 10:44:31 +0530

    Added script to process data, updated README.md, added CodeBook.md file

---
+++ CodeBook.md
@@ -0,0 +1,100 @@
+# CodeBook
+
+## Raw Datasource
+Raw data is downloaded from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip.
+
+A full description is available at the site where the data was obtained:
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+Detailed information on raw data is present in features_info.txt in "UCI HAR Dataset" folder in dataset zip file.
+
+## Tidy Data
+run_analysis.R creates tidydata.csv file.
+
+### Data fields
+Mean and Standard deviation fields from raw data are considered for tidy data activity. 
+tidydata.csv file has average of mean and standard deviation for all measurements in test and train dataset in raw data
+Following data fields are present in tidydata.csv
+ 
+* Subject
+* Activity
+* ActivityName
+* tBodyAccMeanX
+* tBodyAccMeanY
+* tBodyAccMeanZ
+* tBodyAccStdX
+* tBodyAccStdY
+* tBodyAccStdZ
+* tGravityAccMeanX
+* tGravityAccMeanY
+* tGravityAccMeanZ
+* tGravityAccStdX
+* tGravityAccStdY
+* tGravityAccStdZ
+* tBodyAccJerkMeanX
+* tBodyAccJerkMeanY
+* tBodyAccJerkMeanZ
+* tBodyAccJerkStdX
+* tBodyAccJerkStdY
+* tBodyAccJerkStdZ
+* tBodyGyroMeanX
+* tBodyGyroMeanY
+* tBodyGyroMeanZ
+* tBodyGyroStdX
+* tBodyGyroStdY
+* tBodyGyroStdZ
+* tBodyGyroJerkMeanX
+* tBodyGyroJerkMeanY
+* tBodyGyroJerkMeanZ
+* tBodyGyroJerkStdX
+* tBodyGyroJerkStdY
+* tBodyGyroJerkStdZ
+* tBodyAccMagMean
+* tBodyAccMagStd
+* tGravityAccMagMean
+* tGravityAccMagStd
+* tBodyAccJerkMagMean
+* tBodyAccJerkMagStd
+* tBodyGyroMagMean
+* tBodyGyroMagStd
+* tBodyGyroJerkMagMean
+* tBodyGyroJerkMagStd
+* fBodyAccMeanX
+* fBodyAccMeanY
+* fBodyAccMeanZ
+* fBodyAccStdX
+* fBodyAccStdY
+* fBodyAccStdZ
+* fBodyAccMeanFreqX
+* fBodyAccMeanFreqY
+* fBodyAccMeanFreqZ
+* fBodyAccJerkMeanX
+* fBodyAccJerkMeanY
+* fBodyAccJerkMeanZ
+* fBodyAccJerkStdX
+* fBodyAccJerkStdY
+* fBodyAccJerkStdZ
+* fBodyAccJerkMeanFreqX
+* fBodyAccJerkMeanFreqY
+* fBodyAccJerkMeanFreqZ
+* fBodyGyroMeanX
+* fBodyGyroMeanY
+* fBodyGyroMeanZ
+* fBodyGyroStdX
+* fBodyGyroStdY
+* fBodyGyroStdZ
+* fBodyGyroMeanFreqX
+* fBodyGyroMeanFreqY
+* fBodyGyroMeanFreqZ
+* fBodyAccMagMean
+* fBodyAccMagStd
+* fBodyAccMagMeanFreq
+* fBodyBodyAccJerkMagMean
+* fBodyBodyAccJerkMagStd
+* fBodyBodyAccJerkMagMeanFreq
+* fBodyBodyGyroMagMean
+* fBodyBodyGyroMagStd
+* fBodyBodyGyroMagMeanFreq
+* fBodyBodyGyroJerkMagMean
+* fBodyBodyGyroJerkMagStd
+* fBodyBodyGyroJerkMagMeanFreq
---
+++ run_analysis.R
@@ -0,0 +1,83 @@
+library(data.table)
+
+downloadAndUnzipFile <- function(url) {
+  if(!file.exists("UCI HAR Dataset")) {
+    download.file(url,"data.zip","curl")
+    unzip("data.zip")
+  }
+}
+
+loadRequiredFeatures <- function(features) {
+  grep(".*mean.*|.*std.*",features[,2])
+}
+
+loadRequiredFeaturesNames <- function(features, requiredFeatures) {
+  names <- features[requiredFeatures,2]
+  names <- gsub('-mean\\(\\)', 'Mean', names)
+  names <- gsub('-std\\(\\)', 'Std', names)
+  names <- gsub('-meanFreq\\(\\)', 'MeanFreq', names)
+  names <- gsub('-', '', names)
+  names
+}
+
+loadTrainData <- function(featuresRequired) {
+  train <- read.table("UCI HAR Dataset/train/X_train.txt")[featuresRequired]
+  trainActivities <- read.table("UCI HAR Dataset/train/y_train.txt")
+  trainSubjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
+  
+  cbind(trainSubjects, trainActivities, train)
+}
+
+loadTestData <- function(featuresRequired) {
+  test <- read.table("UCI HAR Dataset/test/X_test.txt")[featuresRequired]
+  testActivites <- read.table("UCI HAR Dataset/test/y_test.txt")
+  testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
+  
+  cbind(testSubjects, testActivites, test)
+}
+
+activityName <- function(activityIndex, activityLabels) {
+  activityLabels[activityIndex,2]
+}
+
+downloadAndUnzipFile("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip")
+
+activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt", stringsAsFactors = FALSE)
+features <- read.table("UCI HAR Dataset/features.txt", stringsAsFactors = FALSE, strip.white = TRUE)
+
+# Required features
+requiredFeatures <- loadRequiredFeatures(features)
+
+# Required features names
+requiredFeaturesNames <- loadRequiredFeaturesNames(features,requiredFeatures)
+
+# Load train data
+trainData <- loadTrainData(requiredFeatures)
+
+# Load test data
+testData <- loadTestData(requiredFeatures)
+
+# Merge data
+completeData <- rbind(trainData, testData)
+
+# Add column names
+colnames(completeData) <- c("Subject", "Activity", requiredFeaturesNames)
+
+# Tidy data
+completeData <- data.table(completeData)
+
+# Calculate mean by Subject and Activity
+tidydata <- completeData[,lapply(.SD,mean), keyby=.(Subject,Activity)]
+
+# Add Activity Names for readability
+tidydata <- tidydata[,ActivityName:=activityName(Activity,activityLabels)]
+
+# Re-order columns
+setcolorder(tidydata, c("Subject","Activity","ActivityName",colnames(dt)[!(colnames(dt) %in% c("Subject","Activity","ActivityName"))]))
+
+# Write data in CSV format
+if(file.exists("tidydata.csv")) {
+  file.remove("tidydata.csv")
+}
+
+fwrite(tidydata, "tidydata.csv")
